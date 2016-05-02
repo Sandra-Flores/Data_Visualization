66bad15679fc2161e6e52b35b0cf9d7786ee7594
alexmorales26 <alexmorales@csumb.edu>
2016-05-02 14:12:02 -0700

    gui
    choose city and coordinates

---
+++ project_3.py
@@ -0,0 +1,7 @@
+from easygui import *
+
+msg = "Please choose a city to lurk on ? "
+title = " lurkergram "
+choices = ["Los Angeles","San Francisco","Fresno","Bakersfield","Santa Cruz","Chico","Barstow","Blythe","Burbank","Concord","Covina","Cresent City","Downey","El Cajon","El Cerrito","Escondido","Eureka","Fairfield","Hamilton","Laguna Beach","Livermore","Lompoc","Long Beach","Merced","Modesto","Monterey","Napa","Needles","Oakland","Oceanside","Ontario","Oxnard","Palmdale","Palm Springs","Pasadena","Petaluma","Pomona","Redding","Redlands","Richmond","Riverside","Sacramento","Salinas","San Bernardino","San Diego","San Fernando","San Jose","San Luis Obispo","Santa Ana","Santa Barbara","Santa Maria","Santa Monica","Santa Paula","Santa Rosa","Stockton","Ukiah","Visalia","Yreka","Yuba City"]
+choice = choicebox (msg,title, choices)
+
